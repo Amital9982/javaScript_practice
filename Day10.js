f0197e3697880d3ead9d7be7c38d8c80dd2070f5
arpit <arpitmittal050603@gmail.com>
2023-12-29 00:39:30 +0530

    classes

--- Day10.js
+++ Day10.js
@@ -36,3 +36,38 @@ const showDateTime = () => {
     const fullTime = dateMonthYear + ' ' + time
     return fullTime
 }
+for (let i = 0; i < n; i++) {
+    console.log()
+}
+
+// declaring an array variable
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+// iterating an array using regular for loop
+let len = names.length;
+for (let i = 0; i < len; i++) {
+    console.log(names[i].toUpperCase())
+}
+
+
+// iterating an array using for of
+for (const name of names) {
+    console.log(name.toUpperCase())
+}
+
+// iterating array using forEach 
+names.forEach((name) => name.toUpperCase())
+
+
+const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'MongoDB', 'Python', 'D3.js'],
+    isMarried: true
+}
+for (const key in person) {
+    console.log(key)
+}
